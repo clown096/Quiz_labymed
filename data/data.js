f6435c4729820eacaa0368195d7ae37724d44b06
clown096 <113256840+clown096@users.noreply.github.com>
2022-09-25 21:53:15 -0600

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+      question: "¿Cúantos años cumple Labymed?",
+      choices: ["21", "24", "20", "22"],
+      answer: "20",
+    },
+    {
+      question: "¿Quíen fundo Labymed?",
+      choices: ["Charles Darwin", "Napoleón Bonaparte", "Julio César", "Aristóteles"],
+      answer: "Aristóteles",
+    },
+    {
+      question: "¿Con qué color identificas a Labymed?",
+      choices: ["Azúl", "Rojo", "Verde", "Amarillo"],
+      answer: "Azúl",
+    },
+  ];
